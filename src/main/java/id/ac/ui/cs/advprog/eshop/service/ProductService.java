0302324f2d454743ed9f19ec7950638698444a3a
Muhammad Reyhan Ardian <reyhanardian16@gmail.com>
2025-02-19 21:54:59 +0700

    delete-product

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import java.util.List;
+
+public interface ProductService {
+    public Product create(Product product);
+    public Product delete(Product product);
+    public Product findById(String id);
+    public List<Product> findAll();
+}
