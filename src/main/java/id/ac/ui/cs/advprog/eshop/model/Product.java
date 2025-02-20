db3deceb5a680ffef2d8a8f7691e8e7bfe5f7cea
Muhammad Reyhan Ardian <reyhanardian16@gmail.com>
2025-02-20 17:41:40 +0700

    Add edit & delete unit tests

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -3,13 +3,20 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import java.util.UUID;
 
 @Getter @Setter
 public class Product {
+
+    @NotBlank(message = "Product name must not be empty")
     private String productName;
+
     private String productId;
-    private int productQuantity;
+
+    @Min(value = 0, message = "Product quantity must not be negative")
+    private Integer productQuantity;
 
     public Product () {
         productId = UUID.randomUUID().toString();
